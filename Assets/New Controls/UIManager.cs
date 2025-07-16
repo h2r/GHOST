f38eeb2f4146658e149235ef16e7f060b949919a
kamyaraman <kamya@raman-home.com>
2025-07-16 13:19:04 -0400

    add pause mode etc

--- Assets/New Controls/UIManager.cs
+++ Assets/New Controls/UIManager.cs
@@ -3,8 +3,20 @@ using UnityEngine;
 public class UIManager : MonoBehaviour
 {
     public ControllerFlow leftFlow;
-
     public ButtonList leftSpotList, leftControlList;
+
+    private bool isOpen = true;
+
+    public void Update()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.Four))
+        {
+            isOpen = !isOpen;
+            print("menu open: " + isOpen);
+            transform.parent.gameObject.GetComponent<Canvas>().enabled = isOpen;
+            leftFlow.SetPaused(isOpen);
+        }
+    }
 
     public bool TryRaycastHover(GameObject hit)
     {
@@ -16,5 +28,10 @@ public class UIManager : MonoBehaviour
     {
         _ = leftSpotList.PressButton(hit, m => leftFlow.SetSpot((SpotMode)m)) ||
         leftControlList.PressButton(hit, m => leftFlow.SetControl((NewControlMode)m));
+    }
+
+    public bool GetOpen()
+    {
+        return isOpen;
     }
 }
