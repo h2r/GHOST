3d908fe350e203a3dfa8683584d11657436d6945
sjuknelis <simonas_juknelis@brown.edu>
2025-08-12 17:46:39 -0400

    adjust position presets

--- Assets/SCOUT/Control Modes/PositionPresetController.cs
+++ Assets/SCOUT/Control Modes/PositionPresetController.cs
@@ -30,23 +30,23 @@ public class PositionPresetController : MonoBehaviour
         switch (presetOrder[curPresetIndex])
         {
             case Preset.BetweenSpots:
-                cameraPosition = (spotOne.transform.position + spotTwo.transform.position) / 2;
+                cameraPosition = (spotOne.transform.position + spotTwo.transform.position) / 2 - new Vector3(0, 0, 5);
                 break;
 
             case Preset.BehindSpotOne:
-                cameraPosition = spotOne.transform.position + new Vector3(0, 0, 1);
+                cameraPosition = spotOne.transform.position - new Vector3(0, 0, 6.5f);
                 break;
 
             case Preset.BehindSpotTwo:
-                cameraPosition = spotTwo.transform.position + new Vector3(0, 0, 1);
+                cameraPosition = spotTwo.transform.position - new Vector3(0, 0, 6.5f);
                 break;
 
             case Preset.ArmSpotOne:
-                cameraPosition = spotOneArm.transform.position;
+                cameraPosition = spotOneArm.transform.position - new Vector3(0, 0, 4.5f);
                 break;
 
             case Preset.ArmSpotTwo:
-                cameraPosition = spotTwoArm.transform.position;
+                cameraPosition = spotTwoArm.transform.position - new Vector3(0, 0, 4.5f);
                 break;
         }
 
