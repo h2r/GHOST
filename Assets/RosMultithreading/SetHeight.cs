f0c78f98193868d3723de5602813eb03d12b5382
Yichen Wei <yichen_wei@brown.edu>
2025-10-02 10:35:47 -0400

    heigh adjusting

---
+++ Assets/RosMultithreading/SetHeight.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using RosSharp.RosBridgeClient.MessageTypes.Spot;
+using RosSharp.RosBridgeClient.MessageTypes.Std;
+using System;
+
+namespace RosSharp.RosBridgeClient
+{
+    public class SetHeight : MonoBehaviour
+    {
+        private RosConnector rosConnector;
+        public string serviceName = "/set_stand_height";
+
+        void Start()
+        {
+            rosConnector = GetComponent<RosConnector>();
+        }
+
+        public void SetHeightPercentage(float height)
+        {
+            SetStandHeightRequest request = new SetStandHeightRequest(height);
+            Debug.Log($"Requested height percentage: {height}");
+            Debug.Log("Service name: " + serviceName);
+
+            rosConnector.RosSocket.CallService<SetStandHeightRequest, SetStandHeightResponse>(
+                serviceName,
+                response => Debug.Log($"Set Height Service response received: success={response.success}, message={response.message}"),
+                request
+            );
+
+            Debug.Log($"Requested to set height to {height * 100}%");
+        }
+    }
+}
