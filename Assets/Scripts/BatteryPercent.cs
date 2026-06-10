da8247616f5dc200b48b897928b781af89c4e199
n-7jxh <nyjah_harrison@brown.edu>
2026-06-10 15:54:39 -0400

    Update BatteryPercent.cs

--- Assets/Scripts/BatteryPercent.cs
+++ Assets/Scripts/BatteryPercent.cs
@@ -20,20 +20,20 @@ namespace RosSharp.RosBridgeClient
 
         protected override void Start()
         {            
-            // Debug.Log("[BatteryPercent] FloatSubscriber.Start() called");
+            Debug.Log("[BatteryPercent] FloatSubscriber.Start() called");
             base.Start();
-            // Debug.Log("[BatteryPercent] Subscriber started with topic " + Topic);
+            Debug.Log("[BatteryPercent] Subscriber started with topic " + Topic);
         }
 
 
         protected override void ReceiveMessage(BatteryStateArray message)
         {
-            // Debug.Log("[BatteryPercent] ReceiveMessage() triggered!");
+            Debug.Log("[BatteryPercent] ReceiveMessage() triggered!");
 
             // Just take first battery charge_percentage for now
             batteryLevel = message.battery_states[0].charge_percentage;
-            // Debug.Log($"[BatteryPercent] Updated batteryLevel: {batteryLevel}");
-            // Debug.Log("[BatteryPercent] " + message);
+            Debug.Log($"[BatteryPercent] Updated batteryLevel: {batteryLevel}");
+            Debug.Log("[BatteryPercent] " + message);
 
         }
 
@@ -45,12 +45,12 @@ namespace RosSharp.RosBridgeClient
             {
                 if (batteryLevel < 0)
                 {
-                    // Debug.Log($"[{spotIdentifier}] BatteryLevel is invalid (<0) → Displaying 'No Data'");
+                    Debug.Log($"[{spotIdentifier}] BatteryLevel is invalid (<0) → Displaying 'No Data'");
                     batteryText.text = $"{spotIdentifier} Spot Battery: No Data";
                 }
                 else
                 {
-                    // Debug.Log($"[{spotIdentifier}] BatteryLevel: {batteryLevel:F1}% → Displaying value");
+                    Debug.Log($"[{spotIdentifier}] BatteryLevel: {batteryLevel:F1}% → Displaying value");
                     batteryText.text = $"{spotIdentifier} Spot Battery: {batteryLevel:F1}%";
                 }
             }
