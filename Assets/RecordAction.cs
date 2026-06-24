3ce72a964187fe9c5f3d3f5bc93a760d5899e384
n-7jxh <nyjah_harrison@brown.edu>
2026-06-24 15:27:04 -0400

    Revert "Update RecordAction.cs"
    This reverts commit f4c944f69771fb61da33c27e19a1d55a27b0052b.

--- Assets/RecordAction.cs
+++ Assets/RecordAction.cs
@@ -1,43 +1,13 @@
 using UnityEngine;
-using System;
-using System.Collections;
 
 public class RecordAction : UIOption
 {
     public SpotMode spot;
     public string spot_name = "Spot 1"; //may or not need these; grabbed from StowArmsButton
-    private bool timerActive;
-    private float elapsedTime;
-    private const float maxRecordingTime = 900f; //15 min to complete task
-
 
     public override void DoAction(ScoutModeManager modeManager)
     {
-       timerActive = true;
-       elapsedTime = 0f;
-    }
-
-    private void Update()
-    {
-        if (!timerActive) return; 
-
-        elapsedTime += Time.deltaTime;
-
-        //put ROS logic here later
        
-
-        if (elapsedTime >= maxRecordingTime)
-        {
-            StopRecording();
-        }
-    }
-
-    private void StopRecording()
-    {
-        timerActive = false;
-        elapsedTime = 0f;
-        
-        //put ending logic here
     }
 
     public override string GetName()
