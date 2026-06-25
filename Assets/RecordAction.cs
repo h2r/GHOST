2fe682cfa09c7d630dac96eef075966b214d9844
n-7jxh <nyjah_harrison@brown.edu>
2026-06-25 16:34:00 -0400

    rewiring the UI tabs + adding RecordAction

---
+++ Assets/RecordAction.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using System;
+using System.Collections;
+
+public class RecordAction : UIOption
+{
+    public SpotMode spot;
+    public string spot_name = "Spot 1"; //may or not need these; grabbed from StowArmsButton
+    private bool timerActive;
+    private float elapsedTime;
+    private const float maxRecordingTime = 900f; //15 min to complete task
+
+
+    public override void DoAction(ScoutModeManager modeManager)
+    {
+       timerActive = true;
+       elapsedTime = 0f;
+       Debug.Log("Button Pressed!"); //remove once actual logic is here
+    }
+
+    private void Update()
+    {
+        if (!timerActive) return; 
+
+        elapsedTime += Time.deltaTime;
+
+        //put ROS logic here later
+        Debug.Log("Pretend I'm recording..."); //remove for actual logic
+        if (elapsedTime >= maxRecordingTime) 
+        {
+            StopRecording();
+        }
+    }
+
+    private void StopRecording()
+    {
+        timerActive = false;
+        elapsedTime = 0f;
+        
+        //put ending logic here
+    }
+
+    public override string GetName()
+    {
+        return $"Record Action";
+    }
+
+    public override Color GetSelectedColor()
+    {
+        return Color.white;
+    }
+}
+
